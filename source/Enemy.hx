0fefc677d2ba436aaed13cba2f481b2bc83b9aef
Alejandro Ramirez <alejandroid@apple.com>
2024-07-15 14:35:01 -0500

    Removing unused imports from Enemy.hx

--- source/Enemy.hx
+++ source/Enemy.hx
@@ -1,11 +1,7 @@
 package;
 
-import flixel.FlxG;
-import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.math.FlxPoint;
-import flixel.math.FlxVelocity;
-import flixel.system.FlxSound;
 
 using flixel.util.FlxSpriteUtil;
 
